a92fd7e5e3e05c52d80ecce9e0408f7aa9b501e1
VictorHMSforne <vmendessforne@gmail.com>
2026-08-13 22:06:50 -0300

    Aprendendo sobre os tipos de encapsulamento

--- primeiraClasse/src/main/java/br/edu/Unicesumar/Veiculo.java
+++ primeiraClasse/src/main/java/br/edu/Unicesumar/Veiculo.java
@@ -11,22 +11,36 @@ public class Veiculo {
         this.modelo = modelo;
         this.anoFabricacao = anoFabricacao; // o this. faz referência a variável da classe
         this.qtdPortas = 2;
+        validarAnoFabricacao(anoFabricacao);
     }
 
-    public int verQtdPortas(){
+    public int getQtdPortas(){
         return qtdPortas;
     }
 
-    public void alterarQtdPortas(int valorNovo){
-        qtdPortas = valorNovo;
+    public String getMarca(){
+        return marca;
+    }
+    public void setQtdPortas(int qtdPortas){
+        if (qtdPortas > 0){
+            this.qtdPortas = qtdPortas;
+        }
     }
 
-
-
+    public void setAnoFabricacao(int anoFabricacao){
+        validarAnoFabricacao(anoFabricacao);
+    }
 
     public void exibirDados(){
         System.out.printf("Marca %s | Modelo: %s | Ano de Fabricação: %d | Quantidade de portas: %d\n",
                 this.marca, modelo, anoFabricacao, qtdPortas); // O This pode ser usado para deixar o código mais limpo e auxiliar a entender que essa variável é da classe
+    }
+
+    private void validarAnoFabricacao(int anoFabricacao){ //SOMENTE AQUI USA NA CLASSE, se eu instanciar não tem como usar. Ou seja quem usa esse carinha é o set
+        if (anoFabricacao <= 1500){
+            System.out.println("Valor inválido!");
+        }
+        setAnoFabricacao(anoFabricacao);
     }
 }
 
