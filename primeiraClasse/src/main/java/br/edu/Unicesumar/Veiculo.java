94129a89ad93a3effe4f8909a3eb5ed3ea85de2f
VictorHMSforne <vmendessforne@gmail.com>
2026-08-13 21:56:49 -0300

    Terminando a parte de métodos

--- primeiraClasse/src/main/java/br/edu/Unicesumar/Veiculo.java
+++ primeiraClasse/src/main/java/br/edu/Unicesumar/Veiculo.java
@@ -1,19 +1,32 @@
 package br.edu.Unicesumar;
 
 public class Veiculo {
-    public String modelo;
-    public String marca;
-    public int anoFabricacao;
+    private String modelo;
+    private String marca;
+    private int anoFabricacao;
+    private int qtdPortas;
 
     public Veiculo(String marca, String modelo, int anoFabricacao){ // Esse aqui é o construtor, usado para não inicializar o projeto vazio
         this.marca = marca;
         this.modelo = modelo;
         this.anoFabricacao = anoFabricacao; // o this. faz referência a variável da classe
+        this.qtdPortas = 2;
     }
 
+    public int verQtdPortas(){
+        return qtdPortas;
+    }
+
+    public void alterarQtdPortas(int valorNovo){
+        qtdPortas = valorNovo;
+    }
+
+
+
+
     public void exibirDados(){
-        System.out.printf("Marca %s | Modelo: %s | Ano de Fabricação: %d \n",
-                this.marca, modelo, anoFabricacao); // O This pode ser usado para deixar o código mais limpo e auxiliar a entender que essa variável é da classe
+        System.out.printf("Marca %s | Modelo: %s | Ano de Fabricação: %d | Quantidade de portas: %d\n",
+                this.marca, modelo, anoFabricacao, qtdPortas); // O This pode ser usado para deixar o código mais limpo e auxiliar a entender que essa variável é da classe
     }
 }
 
