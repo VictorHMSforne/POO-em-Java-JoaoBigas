c6bf6ec110a67724df01cc1082ed8e895776313a
VictorHMSforne <vmendessforne@gmail.com>
2026-08-16 17:07:57 -0300

    Terminado exercício 2 sobre operações matemáticas simples

--- Exercises_Java/src/main/java/br/com/Unicesumar/Exc_2.java
+++ Exercises_Java/src/main/java/br/com/Unicesumar/Exc_2.java
@@ -1,7 +1,99 @@
 package br.com.Unicesumar;
 
+import java.util.Scanner;
+
 public class Exc_2 {
     public static void main(String[] args) {
-        System.out.println("teste");
+        /*
+        =============================================
+        Exercícios 2: Operações Matemáticas Simples
+        =============================================
+
+        ============
+        2.6
+        ============
+
+
+        System.out.println("Digite um número");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.printf("O Número escolhido foi: %d\n \n",number);
+        System.out.printf("O Dobro desse número: %d\n", number*2);
+        System.out.printf("O Triplo desse número: %d\n", number*3);
+        System.out.printf("A Raiz quadrada desse número: %.2f", Math.sqrt(number));
+
+        ============
+        2.7
+        ============
+
+        System.out.println("==================\n");
+        System.out.println("Conversão de MONEY\n");
+        System.out.println("==================\n");
+
+        System.out.println("Digite um valor fixo da taxa de câmbio\n");
+        System.out.print("1$ -> R$ ");
+        Scanner scan = new Scanner(System.in);
+        float taxaCambio = scan.nextFloat();
+
+        System.out.print("\nAgora digite quantos R$ para a conversão: R$ ");
+        float moneyBR = scan.nextFloat();
+
+        System.out.printf("R$ %.2f equivale a: %.2f $",moneyBR, moneyBR/taxaCambio );
+
+        ============
+        2.8
+        ============
+
+        System.out.println("==================\n");
+        System.out.println("     POTÊNCIA     \n");
+        System.out.println("==================\n");
+
+        System.out.print("Digite um n°: ");
+        Scanner scan = new Scanner(System.in);
+        double number = scan.nextDouble();
+
+        System.out.printf("\nO Quadrado desse n°: %.2f", Math.pow(number,2.00));
+        System.out.printf("\nO Cubo desse n°: %.2f", Math.pow(number,3.00));
+
+        ============
+        2.9
+        ============
+
+        System.out.println("===================\n");
+        System.out.println("   MAIOR E MENOR   \n");
+        System.out.println("===================\n");
+
+        System.out.print("Digite o 1° número: ");
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();
+
+        System.out.print("\nDigite o 2° número: ");
+        int n2 = scan.nextInt();
+
+        System.out.printf("O Maior entre eles é: %d\n", Math.max(n1,n2));
+        System.out.printf("O Menor entre eles é: %d", Math.min(n1,n2));
+
+        ============
+        2.10
+        ============
+
+
+        System.out.println("============================\n");
+        System.out.println("   Positivo, Negativo e 0   \n");
+        System.out.println("============================\n");
+
+        System.out.print("Digite um n°: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        if(number > 0){
+            System.out.println("\nO Número digitado é positivo");
+        } else if (number == 0 ) {
+            System.out.println("\nO Número digitado é 0");
+        }
+        else
+            System.out.println("O Número digitado é negativo");
+
+         */
     }
 }
